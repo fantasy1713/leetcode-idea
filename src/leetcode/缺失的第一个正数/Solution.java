a23cc8e1d53a3727b384b8ad0e56e59263b72609
fantasy1713 <fantasy1713@qq.com>
2020-02-11 23:32:55 +0800

    【增加】AC

--- src/leetcode/缺失的第一个正数/Solution.java
+++ src/leetcode/缺失的第一个正数/Solution.java
@@ -2,15 +2,18 @@ package leetcode.缺失的第一个正数;
 
 public class Solution {
     public int firstMissingPositive(int[] nums) {
-        int biggest = 0;
+        int positiveCount = 0;
+/*
+当nums中有n个正数，那么结果可定在[1,n+1]中，所以只需要创建n+1大小的数组即可
+ */
         for(int i = 0; i<nums.length;i++){
-            if(nums[i]>biggest){
-                biggest = nums[i];
+            if(nums[i]>0){
+                positiveCount++;
             }
         }
-        byte[] map = new byte[biggest];
+        byte[] map = new byte[positiveCount+1];
         for(int i = 0; i<nums.length;i++){
-            if(nums[i]>0 && nums[i]!=biggest){
+            if(nums[i]>0  && nums[i]<map.length){
                 map[nums[i]] = 0x01;
             }
 
